7a3ade8a3060b95788581de2f580445d61b88ad2
Gautam Simplia <gautam_simplia@outlook.com>
2025-02-21 00:04:42 +0530

    fetch draft & trash post

--- react-app/src/components/Post/PostList.jsx
+++ react-app/src/components/Post/PostList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import AddPostModal from './Modal/AddPostModal'
 import EditPostModal from './Modal/EditPostModal'
 
@@ -6,6 +6,7 @@ export default function PostList() {
 
     const [isAddPostModalOpen, setIsAddPostModalOpen] = useState(false)
     const [isEditPostModalOpen, setIsEditPostModalOpen] = useState(false)
+    const [posts, setPosts] = useState([])
 
     const togglePostModal = () => {
       setIsAddPostModalOpen(prevState => !prevState);
@@ -15,8 +16,56 @@ export default function PostList() {
       setIsEditPostModalOpen(prevState => !prevState);
     }
 
+    // list of wordpress posts
+  const fetchWordpressPosts = async () => {
+    try {
+      const res = await fetch('http://localhost/spa-app/wp/wp-json/wp/v2/posts?status=publish,draft,trash', {
+        method: 'GET',
+        headers: {
+          'Authorization': 'Basic ' + btoa("admin:admin")
+        }
+      });
+  
+      if (!res.ok) {
+        const errorData = await res.json();
+        console.error('Error fetching posts:', errorData);
+        return;
+      }
+  
+      const data = await res.json();
+      setPosts(data);
+      console.log(data);
+    } catch (error) {
+      console.error('Fetch error:', error);
+    }
+  };
+
+    useEffect(() => {
+        fetchWordpressPosts()
+      }, [])
+
+    // post status 
+    const postStatus = (status) => {
+      
+      if(status === 'draft') {
+        return  <button className="border px-4 py-2 bg-orange-500 text-white">{status.toUpperCase()}</button>
+
+      } else if(status === 'trash') {
+        return  <button className="border px-4 py-2 bg-red-500 text-white">{status.toUpperCase()}</button>
+      }
+      else if(status === 'publish') {
+        return  <button className="border px-4 py-2 bg-green-500 text-white">{status.toUpperCase()}</button>
+      }
+
+    }
+    // get post categories
+    const getPostCategories = (categories) => {
+      const categoryNames = categories.map((cat) => cat.name);
+      return categoryNames.join(', ');
+    }
+
   return (
-    <>    
+    <>
     <div className="container max-w-5xl mx-auto p-4">
     <div className="loader"></div> 
 
@@ -37,23 +86,27 @@ export default function PostList() {
         </tr>
       </thead>
       <tbody>
-        <tr className="text-center">
-          <td className="border px-4 py-2">1</td>
-          <td className="border px-4 py-2">Sample Title</td>
+        {
+            posts.map((post, index) => (
+                <tr key={index} className="text-center">
+          <td className="border px-4 py-2">{post.id}</td>
+          <td className="border px-4 py-2">{post.title.rendered}</td>
           <td className="border px-4 py-2">
-            <button className="border px-4 py-2 bg-green-500 text-white">publish</button>
+              {postStatus(post.status)}
           </td>
-          <td className="border px-4 py-2">Loading...</td>
+           <td className="border px-4 py-2">{getPostCategories(post.categories)}</td>
           <td className="border px-4 py-2">
-            <img src="http://localhost/learrn/cms/wp-content/uploads/2024/10/No-Image-Placeholder.png" alt="Featured" className="w-16 h-16 object-cover" />
+            <img src={post.featured_media} alt={post.featured_media} className="w-16 h-16 object-cover" />
           </td>
           <td className="border px-4 py-2">
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded mr-2 cursor-pointer" onClick={toggleEditPostModal}>Edit</button>
             <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-3 rounded cursor-pointer" disabled>Delete</button>
           </td>
         </tr>
+        ))
+       }
        </tbody>
-    </table>
+    </table>  
     {
       isAddPostModalOpen && (
         <AddPostModal handleCloseEvent={togglePostModal} />
